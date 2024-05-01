86477acab1f16eb1eecaf97262911d0a5362ca06
dinozoff1981 <tensaeg@yahoo.com>
2024-05-01 03:26:20 +0200

    AiResponse Updated

--- TravelGenie-WeB-APP.Server/Services/ITravel.cs
+++ TravelGenie-WeB-APP.Server/Services/ITravel.cs
@@ -1,9 +1,11 @@
-﻿namespace TravelGenie_WeB_APP.Server.Services
+﻿using TravelGenie_WeB_APP.Server.Dtos;
+
+namespace TravelGenie_WeB_APP.Server.Services
 {
     public interface ITravel
     {
         Task<List<string>> GetCountryNames();//done
         Task<List<string>> GetCities(string country);//done
-        Task<string> GetPlanDetail(string propt);
+        Task<ChatGptDto> GetPlanDetail(string propt);
     }
 }
